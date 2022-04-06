c5e50bee949b7ba686732aea10244281ce1db719
twinh <twinhuang@qq.com>
2022-04-06 23:34:59 +0800

    style(mxjs-app): Missing return type on function
    13
    Warning:   1:24  warning  Missing return type on function  @typescript-eslint/explicit-module-boundary-types
    14
    Warning:   5:24  warning  Missing return type on function  @typescript-eslint/explicit-module-boundary-types

--- string.ts
+++ string.ts
@@ -1,7 +1,7 @@
-export const lcfirst = (str: string) => {
+export const lcfirst = (str: string): string => {
   return str.charAt(0).toLowerCase() + str.slice(1);
 };
 
-export const ucfirst = (str: string) => {
+export const ucfirst = (str: string): string => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
