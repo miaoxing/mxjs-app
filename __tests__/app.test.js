2bf1630a7c2e1fdd25852573c16c16f582db0a25
twinh <twinhuang@qq.com>
2022-06-01 11:30:25 +0800

    test(mxjs-app): 更新测试支持接口不重写的情况

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,68 +1,47 @@
-import {url} from '../';
+import {url, history} from '../';
 
 describe('app', () => {
-  const originalLocation = window.location;
-
-  beforeEach(() => {
-    delete window.location;
-    window.location = {
-      pathname: '',
-      href: '',
-    };
-  });
-
-  afterEach(() => {
-    window.location = originalLocation;
-  });
-
   test('url', () => {
     expect(url.to('abc')).toBe('/abc');
 
     expect(url.to('abc?a=b')).toBe('/abc?a=b');
   });
 
-  test('generate url with url rewrite', () => {
-    window.location.search = '?r=test';
-
-    expect(url.to('users')).toBe('/?r=users');
-
-    expect(url.to('users', {key: 'value'})).toBe('/?r=users&key=value');
-
-    expect(url.to('users/%s', 1,{key: 'value'})).toBe('/?r=users%2F1&key=value');
-  });
-
   test('api url', () => {
+    url.setOption('apiRewrite', true);
+
     expect(url.api('users')).toBe('/api/users');
 
     expect(url.api('users?a=b')).toBe('/api/users?a=b');
 
-    window.location.pathname = '/admin';
+    const originalPathname = history.location.pathname;
+    history.location.pathname = '/admin';
 
     expect(url.api('users')).toBe('/admin-api/users');
 
     expect(url.api('users?a=b')).toBe('/admin-api/users?a=b');
+
+    url.setOption('apiRewrite', false);
+
+    history.location.pathname = originalPathname;
   });
 
   test('generate api url with url rewrite', () => {
-    window.location.search = '?r=test';
+    expect(url.api('users')).toBe('/index.php?r=api%2Fusers');
 
-    expect(url.api('users')).toBe('/?r=api%2Fusers');
+    expect(url.api('users?a=b')).toBe('/index.php?r=api%2Fusers&a=b');
 
-    expect(url.api('users?a=b')).toBe('/?r=api%2Fusers&a=b');
+    expect(url.api('users?a=b', {key: 'value'})).toBe('/index.php?r=api%2Fusers&a=b&key=value');
 
-    expect(url.api('users?a=b', {key: 'value'})).toBe('/?r=api%2Fusers&a=b&key=value');
-
-    expect(url.api('users/%s?a=b', 1,{key: 'value'})).toBe('/?r=api%2Fusers%2F1&a=b&key=value');
+    expect(url.api('users/%s?a=b', 1, {key: 'value'})).toBe('/index.php?r=api%2Fusers%2F1&a=b&key=value');
   });
 
   test('should overwrite params', () => {
-    window.location.search = '?r=test';
-    expect(url.api('users?a=b', {a: 'c'})).toBe('/?r=api%2Fusers&a=c');
+    expect(url.api('users?a=b', {a: 'c'})).toBe('/index.php?r=api%2Fusers&a=c');
   });
 
   test('should ignore router param', () => {
-    window.location.search = '?r=test';
-    expect(url.api('users?r=test', {a: 'b'})).toBe('/?r=api%2Fusers&a=b');
+    expect(url.api('users?r=test', {a: 'b'})).toBe('/index.php?r=api%2Fusers&a=b');
   });
 });
 
